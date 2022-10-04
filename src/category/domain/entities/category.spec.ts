f4867732e1ae086de6a13799d6dd0617aaa6f078
joaofrncsc <jfrodrigues42@gmail.com>
2022-10-03 21:50:06 -0300

    mais testes

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,24 +1,128 @@
 import { Category } from './category';
 
 describe("Category Unit Tests", ():void => {
-  test("is category constructor", () => {
+  test("category with only the non mandatory prop", () => {
+    const now = new Date();
+    const category = new Category({
+      title: 'category title'
+    });
+
+    expect(category.props).toStrictEqual({
+      title: 'category title',
+      description: null,
+      active: true,
+      createdAt: now
+    });
+  })
+
+  test("category with only the non mandatory prop and the description", () => {
+    const now = new Date();
+    const category = new Category({
+      title: 'another category title',
+      description: 'another category description'
+    });
+
+    expect(category.props).toStrictEqual({
+      title: 'another category title',
+      description: 'another category description',
+      active: true,
+      createdAt: now
+    });
+  })
+
+  test("category with the non mandatory prop and omitting the date", () => {
     const now = new Date();
 
-    const testCategory = new Category({
-      title: 'category title',
-      description: 'category description',
-      active: true,
-      createdAt: now
-    })
+    const category = new Category({
+      title: 'another category title'
+    });
 
-    expect(testCategory.props).toStrictEqual({
-      title: 'category title',
-      description: 'category description',
+    const {createdAt, ...dateOmitted} = category;
+
+    expect(dateOmitted.props).toStrictEqual({
+      title: 'another category title',
+      description: null,
       active: true,
       createdAt: now
-    })
+    });
+  })
+
+  test("is getting the title", () => {
+    const now = new Date();
+
+    const category = new Category({
+      title: 'another category title',
+      active: false,
+      createdAt: now,
+    });
+
+    expect(category.title).toBe('another category title');
+  })
+
+  test("is getting the description", () => {
+    const now = new Date();
+
+    const category = new Category({
+      title: 'another category title',
+      description: 'another category description',
+      active: false,
+      createdAt: now,
+    });
+
+    expect(category.description).toBe('another category description');
+  })
+
+  test("is getting the active property", () => {
+    const now = new Date();
+
+    const category = new Category({
+      title: 'another category title',
+      description: 'another category description',
+      active: false,
+      createdAt: now,
+    });
+
+    expect(category.active).toBeFalsy();
+  })
+
+  test("is getting the createdAt property", () => {
+    const now = new Date();
+
+    const category = new Category({
+      title: 'another category title',
+      description: 'another category description',
+      active: false,
+      createdAt: now,
+    });
+
+    expect(category.createdAt).toBe(now);
+  })
+
+  test("is setting the description property", () => {
+    const now = new Date();
+
+    let category = new Category({
+      title: 'another category title',
+      description: 'another category description',
+      active: false,
+      createdAt: now,
+    });
+
+    category["description"] = null;
+    expect(category.description).toBe(null);
+  })
+  
+  test("is setting the active property", () => {
+    const now = new Date();
+
+    let category = new Category({
+      title: 'another category title',
+      description: 'another category description',
+      active: false,
+      createdAt: now,
+    });
+
+    category["active"] = true;
+    expect(category.active).toBeTruthy();
   })
 });
-
-
-// faça seu teste falhar após ele passar
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,25 +1,27 @@
 export type CategoryProperties = {
- title: String;
- active: Boolean;
- description?: String;
+ title: string;
+ active?: boolean;
+ description?: string;
  createdAt?: Date;
 }
 
 export class Category {
   constructor (public readonly props: CategoryProperties){
     this.description = this.props.description;
+    this.props.active = this.props.active ?? true; 
     this.props.createdAt = this.props.createdAt ?? new Date();
-  };
+
+  }
 
   get title() {
     return this.props.title;
   }
 
-  get description() {
+  get description() { 
     return this.props.description;
   }
 
-  private set description(value: String) {
+  private set description(value: string) {
     this.props.description = value ?? null;
   }
 
@@ -27,11 +29,11 @@ export class Category {
     return this.props.active;
   }
 
-  private set active(value: Boolean) {
+  private set active(value: boolean) {
     this.props.active = value ?? true;
   }
 
   get createdAt() {
-    return this.props.createdAt ?? new Date();;
+    return this.props.createdAt;
   }
 }
