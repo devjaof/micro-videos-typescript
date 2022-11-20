004cc3734d75ba049f4360ae7faaf9536eb06c0b
Joao Francisco <jfrodrigues42@gmail.com>
2022-11-20 12:48:15 +0000

    feat: cria conversao de value para string em todos os casos e cria seus devidos testes

--- src/@seedwork/domain/valueObjects/valueObject.spec.ts
+++ src/@seedwork/domain/valueObjects/valueObject.spec.ts
@@ -3,9 +3,7 @@
 
 import ValueObject from "./valueObject";
 
-class StubValueObject extends ValueObject {
-
-}
+class StubValueObject extends ValueObject {};
 
 describe('Value Object Unit Tests', () => {
   it('should set value', () => {
@@ -14,5 +12,29 @@ describe('Value Object Unit Tests', () => {
 
     vo = new StubValueObject({prop: 'batata'});
     expect(vo.value).toStrictEqual({prop: 'batata'});
+  })
+
+  it('should convert value to string', () => {
+    const date = new Date();
+    let arrange: any = [
+      {received: null, expected: "null"}, 
+      {received: undefined, expected: "undefined"},
+      {received: "", expected: ""}, 
+      {received: "batata", expected: "batata"}, 
+      {received: 3, expected: "3"}, 
+      {received: true, expected: "true"}, 
+      {received: false, expected: "false"}, 
+      {received: date, expected: date.toString()}, 
+      {
+        received: {prop: 'bananinha'},
+        expected: JSON.stringify({prop: 'bananinha'})
+      }
+    ];
+
+    arrange.forEach((value: any) => {
+      const vo: any = new StubValueObject(value.received);
+
+      expect(`${vo}`).toBe(value.expected);
+    })
   })
 })
--- src/@seedwork/domain/valueObjects/valueObject.ts
+++ src/@seedwork/domain/valueObjects/valueObject.ts
@@ -8,4 +8,18 @@ export default abstract class ValueObject<Value = any> {
   get value(): Value {
     return this._value;
   }
+
+  toString = () => {
+    if(typeof this.value !== "object" || this.value === null){
+      try {
+        return this.value.toString();
+      } catch (error) {
+        return this.value + "";
+      }
+    }
+
+    const stringValue =  this.value.toString();
+
+    return stringValue === "[object Object]" ? JSON.stringify(this.value) : stringValue;
+  }
 }
