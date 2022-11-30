2975cb675e2d5d08baacf124ced73bdf8c3623b8
Joao Francisco <jfrodrigues42@gmail.com>
2022-11-30 00:03:43 +0000

    feat: inicio da implementacao do repository da categoria

---
+++ src/category/domain/repository/category.repository.ts
@@ -0,0 +1,6 @@
+import {RepositoryInterface} from  '../../../@seedwork/domain/repository/repositoryContracts';
+import { Category } from '../entities/category';
+
+export default interface CategoryRepository extends RepositoryInterface<Category> {
+   
+}
---
+++ src/category/infra/repository/categoryInMemory.repository.ts
@@ -0,0 +1,8 @@
+import InMemoryRepository from "@seedwork/domain/repository/inMemory.repository";
+import { Category } from "category/domain/entities/category";
+import CategoryRepository from "category/domain/repository/category.repository";
+
+
+class CategoryInMemoryRepository extends InMemoryRepository<Category> implements CategoryRepository{
+
+}
