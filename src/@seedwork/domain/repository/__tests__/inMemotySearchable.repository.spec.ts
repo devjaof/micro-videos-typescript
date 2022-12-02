497f7b24abdf72e7c6ab96a1f9542680f45f182e
joaofranciscor <joao.rodrigues@viasoft.com.br>
2022-12-02 16:41:54 +0000

    feat: pasta para testes inMemory repository e testes searchable

---
+++ src/@seedwork/domain/repository/__tests__/inMemotySearchable.repository.spec.ts
@@ -0,0 +1,117 @@
+import Entity from "../../../../@seedwork/domain/entity/entity";
+import { InMemorySearchableRepository } from "../inMemory.repository";
+
+type StubEntityProps = {
+  title: string;
+  price: number;
+}
+
+class StubEntity extends Entity<StubEntityProps>{
+
+}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['title'];
+
+  protected async applyFilter(items: StubEntity[], filter: string | null): Promise<StubEntity[]> {
+    if(!filter) {
+      return items;
+    }
+
+    return items.filter(item => {
+      return (item.props.title.toLowerCase().includes(filter.toLowerCase()) ||
+        item.props.price.toString() === filter);
+    });
+  }
+}
+
+
+describe('InMemorySearchableRepository unit tests', () => {
+  let repository: StubInMemorySearchableRepository;
+
+  beforeEach(() => (repository = new StubInMemorySearchableRepository()));
+
+  describe('applyFilter method', () => {
+    it('should not filter items when filter is null', async () => {
+      const items = [new StubEntity({title: 'title', price: 54})];
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any);
+      const filteredItems = await repository['applyFilter'](items, null);
+
+      expect(filteredItems).toStrictEqual(items);
+      expect(spyFilterMethod).not.toBeCalled();
+    })
+
+    it('should filter', async () => {
+      const items = [
+        new StubEntity({title: 'title', price: 54}),
+        new StubEntity({title: 'TITLE', price: 0}),
+        new StubEntity({title: 'batatolas', price: 0}),
+      ];
+
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any);
+      let filteredItems = await repository['applyFilter'](items, 'TITLE');
+
+      expect(filteredItems).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1);
+
+      filteredItems = await repository['applyFilter'](items, '54');
+      expect(filteredItems).toStrictEqual([items[0]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+    })
+  })
+
+  describe('applySort method', () => {
+    it("should not sort items", async () => {
+      const items = [
+        new StubEntity({title: 'a', price: 10}),
+        new StubEntity({title: 'b', price: 0}),
+      ]
+
+      let sortedItems = await repository["applySort"](items, null, null);
+
+      expect(JSON.stringify(sortedItems)).toStrictEqual(JSON.stringify(items));
+    })
+
+    it("should sort items", async () => {
+      const items = [
+        new StubEntity({title: "b", price: 8}),
+        new StubEntity({title: "a", price: 8}),
+        new StubEntity({title: "c", price: 8}),
+      ]
+
+      let sortedItems = await repository["applySort"](items, "title", "asc");
+      expect(sortedItems).toStrictEqual([items[1], items[0], items[2]]);
+      
+      sortedItems = await repository["applySort"](items, "title", "desc");
+      expect(sortedItems).toStrictEqual([items[2], items[0], items[1]]);
+    })
+  })
+
+  describe('applyPaginate method',  () => {
+    it('should paginate items', async () => {
+      const items = [
+        new StubEntity({title: "a", price: 8}),
+        new StubEntity({title: "b", price: 8}),
+        new StubEntity({title: "c", price: 8}),
+        new StubEntity({title: "d", price: 8}),
+        new StubEntity({title: "e", price: 8}),
+      ]
+  
+      let paginatedItems = await repository["applyPaginate"](items, 1, 2);
+      expect(paginatedItems).toStrictEqual([items[0], items[1]]);
+
+      paginatedItems = await repository["applyPaginate"](items, 2, 2);
+      expect(paginatedItems).toStrictEqual([items[2], items[3]]);
+
+      paginatedItems = await repository["applyPaginate"](items, 3, 2);
+      expect(paginatedItems).toStrictEqual([items[4]]);
+
+      paginatedItems = await repository["applyPaginate"](items, 4, 2);
+      expect(paginatedItems).toStrictEqual([]);
+    })
+  })
+
+  describe('search method', () => {
+    
+  })
+})
