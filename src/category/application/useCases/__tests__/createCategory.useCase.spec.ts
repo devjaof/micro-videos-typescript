04161cf1d254b245b35e16fef7e17676840d57da
Joao Francisco <jfrodrigues42@gmail.com>
2022-12-04 13:09:34 +0000

    feat: testes unitarios caso de uso create category

---
+++ src/category/application/useCases/__tests__/createCategory.useCase.spec.ts
@@ -0,0 +1,53 @@
+import CategoryInMemoryRepository from "../../../infra/repository/categoryInMemory.repository";
+import CreateCategoryUseCase from "../createCategory.useCase"
+
+describe("createcategory use case unit tests", () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  })
+
+  it('should create a category', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert');
+    let output = await useCase.execute({title: 'titolo tijolo feijão'});
+
+    expect(spyInsert).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual ({
+      id: repository.items[0].id,
+      title: 'titolo tijolo feijão',
+      description: null,
+      active: true,
+      createdAt: repository.items[0].createdAt
+    })
+
+    output = await useCase.execute({
+      title: 'titolo tijolo feijão', 
+      description: 'descriptão'
+    });
+    
+    expect(output).toStrictEqual ({
+      id: repository.items[1].id,
+      title: 'titolo tijolo feijão',
+      description: 'descriptão',
+      active: true,
+      createdAt: repository.items[0].createdAt
+    })
+
+    output = await useCase.execute({
+      title: 'titolo tijolo feijão', 
+      description: 'descriptão',
+      active: false
+    });
+
+    expect(output).toStrictEqual ({
+      id: repository.items[2].id,
+      title: 'titolo tijolo feijão',
+      description: 'descriptão',
+      active: false,
+      createdAt: repository.items[0].createdAt
+    })
+  })
+})
