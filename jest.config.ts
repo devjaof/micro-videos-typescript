b2a6b5939929a25d06865d3b09062efc82f3a4a4
Joao Francisco <jfrodrigues42@gmail.com>
2022-12-05 22:26:56 +0000

    feat: jest coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -43,7 +43,14 @@ export default {
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
