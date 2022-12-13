d276e3876e1b4602e1b4635e1aa0b75880b01b55
joaofranciscor <joao.rodrigues@viasoft.com.br>
2022-12-13 16:00:40 +0000

    feat: configs path com npm workspaces

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -5,7 +5,7 @@ import { Category } from '@jfr/micro-videos/category/domain';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
+
   @Get()
   getHello(): string {
     console.log(Category);
