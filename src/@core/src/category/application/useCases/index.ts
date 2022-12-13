474595fe802be0060ae502e5d9461143c0ebdeea
joaofranciscor <joao.rodrigues@viasoft.com.br>
2022-12-13 16:39:25 +0000

    fix: corrige indexes para category/application

---
+++ src/@core/src/category/application/useCases/index.ts
@@ -0,0 +1,7 @@
+// created from 'create-ts-index'
+
+export * from './createCategory.useCase';
+export * from './deleteCategory.useCase';
+export * from './getCategory.useCase';
+export * from './listCategories.useCase';
+export * from './updateCategory.useCase';
--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Category } from '@jfr/micro-videos/category/domain';
+import { CreateCategoryUseCase } from '@jfr/micro-videos/category/application';
 
 @Controller()
 export class AppController {
