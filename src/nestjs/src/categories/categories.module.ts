c4903cb588f5a0ce2155ec08f5c7f57d96de7a4b
joaofranciscor <joao.rodrigues@viasoft.com.br>
2022-12-20 16:23:32 +0000

    feat: inject use cases no service e uso dos usecases no controller

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,54 +1,59 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-// import {
-//   CreateCategoryUseCase,
-//   DeleteCategoryUseCase,
-//   GetCategoryUseCase,
-//   ListCategoriesUseCase,
-//   UpdateCategoryUseCase,
-// } from '@jfr/micro-videos/category/application';
-// import { CategoryInMemoryRepository } from '@jfr/micro-videos/category/infra';
-// import { CategoryRepository } from '@jfr/micro-videos/category/domain';
-
-import { CreateCategoryUseCase } from '../../../@core/dist/category/application/useCases';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@jfr/micro-videos/category/application';
+import { CategoryInMemoryRepository } from '@jfr/micro-videos/category/infra';
+import { CategoryRepository } from '@jfr/micro-videos/category/domain';
 
 @Module({
   controllers: [CategoriesController],
   providers: [
     CategoriesService,
     {
-      provide: CreateCategoryUseCase.UseCase,
-      useClass: CreateCategoryUseCase.UseCase,
+      provide: 'CategoryRepository',
+      useClass: CategoryInMemoryRepository,
     },
-    // {
-    //   provide: ListCategoriesUseCase.UseCase,
-    //   useFactory: (categoryRepo: CategoryRepository.Repository) => {
-    //     return new ListCategoriesUseCase.UseCase(categoryRepo);
-    //   },
-    //   inject: ['CategoryInMemoryRepository'],
-    // },
-    // {
-    //   provide: GetCategoryUseCase.UseCase,
-    //   useFactory: (categoryRepo: CategoryRepository.Repository) => {
-    //     return new GetCategoryUseCase.UseCase(categoryRepo);
-    //   },
-    //   inject: ['CategoryInMemoryRepository'],
-    // },
-    // {
-    //   provide: UpdateCategoryUseCase.UseCase,
-    //   useFactory: (categoryRepo: CategoryRepository.Repository) => {
-    //     return new UpdateCategoryUseCase.UseCase(categoryRepo);
-    //   },
-    //   inject: ['CategoryInMemoryRepository'],
-    // },
-    // {
-    //   provide: DeleteCategoryUseCase.UseCase,
-    //   useFactory: (categoryRepo: CategoryRepository.Repository) => {
-    //     return new DeleteCategoryUseCase.UseCase(categoryRepo);
-    //   },
-    //   inject: ['CategoryInMemoryRepository'],
-    // },
+    {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new CreateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new ListCategoriesUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: GetCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new GetCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: UpdateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new UpdateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: DeleteCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new DeleteCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryRepository'],
+    },
   ],
 })
 export class CategoriesModule {}
--- src/nestjs/src/categories/categories.service.ts
+++ src/nestjs/src/categories/categories.service.ts
@@ -9,29 +9,30 @@ import {
 
 @Injectable()
 export class CategoriesService {
-  // @Inject(CreateCategoryUseCase.UseCase)
-  // private createUseCase: CreateCategoryUseCase.UseCase;
-  // @Inject(ListCategoriesUseCase.UseCase)
-  // private listUseCase: ListCategoriesUseCase.UseCase;
-  // @Inject(GetCategoryUseCase.UseCase)
-  // private getUseCase: GetCategoryUseCase.UseCase;
-  // @Inject(UpdateCategoryUseCase.UseCase)
-  // private updateUseCase: UpdateCategoryUseCase.UseCase;
-  // @Inject(DeleteCategoryUseCase.UseCase)
-  // private deleteUseCase: DeleteCategoryUseCase.UseCase;
-  // create(input: CreateCategoryUseCase.Input) {
-  //   return this.createUseCase.execute(input);
-  // }
-  // search(input: ListCategoriesUseCase.Input) {
-  //   return this.listUseCase.execute(input);
-  // }
-  // findOne(input: GetCategoryUseCase.Input) {
-  //   return this.getUseCase.execute(input);
-  // }
-  // update(input: UpdateCategoryUseCase.Input) {
-  //   return this.updateUseCase.execute(input);
-  // }
-  // remove(input: DeleteCategoryUseCase.Input) {
-  //   return this.deleteUseCase.execute(input);
-  // }
+  @Inject(CreateCategoryUseCase.UseCase)
+  private createUseCase: CreateCategoryUseCase.UseCase;
+  @Inject(ListCategoriesUseCase.UseCase)
+  private listUseCase: ListCategoriesUseCase.UseCase;
+  @Inject(GetCategoryUseCase.UseCase)
+  private getUseCase: GetCategoryUseCase.UseCase;
+  @Inject(UpdateCategoryUseCase.UseCase)
+  private updateUseCase: UpdateCategoryUseCase.UseCase;
+  @Inject(DeleteCategoryUseCase.UseCase)
+  private deleteUseCase: DeleteCategoryUseCase.UseCase;
+
+  create(input: CreateCategoryUseCase.Input) {
+    return this.createUseCase.execute(input);
+  }
+  search(input: ListCategoriesUseCase.Input) {
+    return this.listUseCase.execute(input);
+  }
+  findOne(input: GetCategoryUseCase.Input) {
+    return this.getUseCase.execute(input);
+  }
+  update(input: UpdateCategoryUseCase.Input) {
+    return this.updateUseCase.execute(input);
+  }
+  remove(input: DeleteCategoryUseCase.Input) {
+    return this.deleteUseCase.execute(input);
+  }
 }
